4cc50080c35e69070aa5d033cda42285254e76f7
Marsudi <marsudi124@gmail.com>
2024-05-24 16:30:06 +0700

    1

--- test_sample.py
+++ test_sample.py
@@ -4,8 +4,8 @@ import main as web
 
 class BasicTests(unittest.TestCase):
 
+# FAIL TEST CASE
     def test_hello_endpoint(self):
-
         response = web.app.test_client().get('/')
         response_data = response.get_json()
         self.assertEqual(response_data['status'], 200)
